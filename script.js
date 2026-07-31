541234e51791fbdbeb37f2ddb72dc4bef1bd2eea
Aamir Sayyad <aamirsayyad6@gmail.com>
2026-07-31 23:33:09 +0530

    Major fix, cleaned the entire code and made it readable and easy to debug, added meaningful names to all function, organized the code

--- script.js
+++ script.js
@@ -1,27 +1,101 @@
-const InputElement = document.getElementById("inputNum");
+//Important Global constants
+
+const InputElement = document.getElementById("inputNum");  //Place where the user inputs size
 const SubmitButton = document.getElementById("submit");
-const ErorrDisplay = document.getElementById("error-msg");
-const BoardDisplay = document.getElementById("board");
+const ErorrDisplay = document.getElementById("error-msg");  //p holding the error message just below the input part
+const BoardDisplay = document.getElementById("board");      //reference to the board container
 const StartButton = document.getElementById("start-anim");
-let BoardMatrix = [];
-let CurrentQueens=[];
-let N = BoardMatrix.length;
+const ResetBoardButton = document.getElementById("reset-board-btn");
+const QueenCountDisplay = document.getElementById("result-placeholder");  //queen count displayed on right card
+const Description = document.getElementById("description"); //hints for the user
+const SliderButton = document.getElementById("slider-btn"); // > button
+const ResultContainer = document.getElementById("result");  //the entire right side card
+
+
+//State Variables
+let rightSideVisible = true; //boolean to check whether right side card is visible or hidden
+let BoardMatrix = [];  //saves the DOM reference of each cell in all as a matrix
+let CurrentQueens = []; //stores the position of currently onboard queens as [i,j]
+let N = BoardMatrix.length; //stores the length of board
+
+
+
+// EVENT LISTENERS SECTION
+
+//Submit Button press and "Enter" keydown logic explained breifly
+/*
+    State explanation: if Input is same as previous N simple do nothing
+    else if the input is unique from before then validate the input, change the description text,
+    reset the Queens on the board and update the Queens count    
+*/
+
 
 //Enter Event Listener
 InputElement.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-        validateInput();
+    if (event.key === "Enter") { //only trigger for "ENTER" press
+        if (Number(InputElement.value) == N) {
+            return;
+        }
+        else if (validateInput()) {
+            Description.textContent = "Place All the Queens!";
+            CurrentQueens = []; //reset current queens
+            updateQueensCount();
+        }
     }
 })
 
 //Submit Button logic
 SubmitButton.addEventListener("click", () => {
-    validateInput();
+
+
+    if (Number(InputElement.value) == N) return;
+    else if (validateInput()) {
+        Description.textContent = "Place All the Queens!";
+        CurrentQueens = []; //reset current queens
+        updateQueensCount();
+    }
+});
+
+//Reset Button logic explained breifly
+/*
+    State explanation: When reset it pressed , the right side is simply changed, the attack indicators on the
+    board are cleared, then the queens are removed from the board to make it like it was intially,
+    CurrentQueens reference array is emptied 
+    queens count is updated
+*/
+
+//Reset Button Logic
+ResetBoardButton.addEventListener("click", () => {
+    Description.textContent = "Place All the Queens!";
+    clearAttackIndicators();
+    CurrentQueens.forEach((queen) => {  //remove the queens class from all current queen cells
+        BoardMatrix[queen[0]][queen[1]].classList.remove("queen");
+    })
+    CurrentQueens = [];  //empty queen references
+    updateQueensCount();    //update the count of queens on board
 })
 
-//input validation
-function validateInput() {
-    const value = InputElement.value;
+
+//Mostly Self-Explanatory
+SliderButton.addEventListener("click", () => {
+    rightSideVisible = !rightSideVisible; //toggle the state simply
+    if (rightSideVisible) {
+        ResultContainer.classList.replace("result-hidden", "result-visible");
+        SliderButton.textContent = ">";
+    }
+    else {
+        ResultContainer.classList.replace("result-visible", "result-hidden");
+        SliderButton.textContent = "<";
+    }
+})
+
+//Calculates the number of queens on board and updates the count on the right result-card
+function updateQueensCount() {
+    QueenCountDisplay.textContent = `${CurrentQueens.length} / ${N}`;
+}
+// INPUT VALIDATION
+function validateInput() {  //used to Build Board only when the input is within valid range
+    const value = Number(InputElement.value);
     if (value <= 3) {
         ErorrDisplay.textContent = "Solution Doesn't Exist!";
         StartButton.classList.replace("show", "hide");
@@ -32,97 +106,136 @@ function validateInput() {
     }
     else {
         ErorrDisplay.textContent = "";
-        StartButton.classList.replace("hide", "show");
-        buildBoard();
+        StartButton.classList.replace("hide", "show");  //show the animation start button
+        buildBoard();                                   //validation successful now build the board 
+        return true;                                    //validation success
     }
+    return false;                                       //validation failed 
 }
 
-//build the inital board
+// BUILDING BOARD
 function buildBoard() {
-    BoardDisplay.replaceChildren("");
-    BoardMatrix = [];
-    CurrentQueens=[];
-    const n = InputElement.value;
+    CurrentQueens = [];                                 //clear the queen references as entire board is being cleared
+    BoardDisplay.replaceChildren("");                   //clear the grid first
+    BoardMatrix = [];                                   //clear the cell references
+
+    N = Number(InputElement.value);                     //set the board size
     let str = "";
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < N; i++) {                       //generating a template for grid css
         str = str + "50px ";
     }
-    BoardDisplay.style.gridTemplateColumns = `${str}`;
-    BoardDisplay.style.gridTemplateRows=`${str}`;
-    for (let i = 0; i < n; i++) {
-        let newRow = [];
-        for (let j = 0; j < n; j++) {
-            const newCell = document.createElement("div");
+    BoardDisplay.style.gridTemplateColumns = `${str}`;  //set the width of the cell
+    BoardDisplay.style.gridTemplateRows = `${str}`;     //set the height of the cell
+    for (let i = 0; i < N; i++) {                       //this 2D loop builds the board
+        let newRow = [];                                //initate empty row
+        for (let j = 0; j < N; j++) {
+            const newCell = document.createElement("div");//create a new column in the row i.e a cell
             newCell.classList.add("cells");
-            if ((i + j) % 2 == 0) {
+            if ((i + j) % 2 == 0) {                     //this is used to alternate betweeen board colors
                 newCell.classList.add("white");
             }
             else {
                 newCell.classList.add("black");
             }
-            newRow.push(newCell);
-            BoardDisplay.appendChild(newCell);
+            newRow.push(newCell);                       //append the cell to the current row
+            BoardDisplay.appendChild(newCell);          //append the cell to DOM to display
         }
-        BoardMatrix.push(newRow);
+        BoardMatrix.push(newRow);                       //push the row to the Matrix holding references
     }
-    BoardDisplay.style.border = "2px solid black";
-    N = BoardMatrix.length;
+    BoardDisplay.style.border = "2px solid black";      //outline
     addCellEventListeners();
-    console.log(BoardMatrix);
 }
 
-function addCellEventListeners() {
+function addCellEventListeners() { //add event listeners to all cells on the board
     for (let i = 0; i < N; i++) {
         for (let j = 0; j < N; j++) {
-            addQueenToggleListener(BoardMatrix[i][j], i, j);
+            BoardMatrix[i][j].addEventListener("click", () => {
+                updateQueens(BoardMatrix[i][j], i, j);  //clicking toggles the queen's presence
+            })
         }
     }
 }
 
-function addQueenToggleListener(target, m, n) {
-    target.addEventListener("click", (event) => {
-        queenSight(target, m, n);
+function updateQueens(target, m, n) {   //validate the queens with its count and provides the toggling behaviour
+    if (!target.classList.contains("queen") && CurrentQueens.length === N) { // check whether we're out of Queens
+        return;
+    }
+
+    target.classList.toggle("queen");           //toggle the queen state of the currently clicked cell
+
+    if (target.classList.contains("queen")) {   //if the cell now contains a queen then
+
+        CurrentQueens.push([m, n]);             //add the current cell to the CurrentQueens reference array
+
+    }
+    else {
+
+        //find the current cell in the CurrentQueens array and remove it as its no longer a queen cell
+        const idx = CurrentQueens.findIndex((queen) => {
+            return queen[0] === m && queen[1] === n;
+        });
+
+        if (idx !== -1) {
+            CurrentQueens.splice(idx, 1); //removal part
+        }
+
+    }
+
+    updateBoardAttacks();               //updates the Attacks by currenly present Queens
+
+    updateQueensCount();                //self-explanatory
+
+    if (CurrentQueens.length === N) {   //if the queens now have reached their max check whether its a win or fail
+        calculateAndDisplayResult();    //then calculate and Display the result
+    }
+    else if (areQueensUnderAttack()) {
+        Description.textContent = "Queens Are Under Attack !";
+    }
+    else {
+        Description.textContent = "Place All the Queens!";
+    }
+    // else {
+    //     Description.textContent = "Place All the Queens!";
+    // }
+
+}
+
+function areQueensUnderAttack() { //by simply checking whether any queen contain danger class
+    let areThey = false;          //assumption
+    CurrentQueens.forEach(([row, col]) => {
+        if (BoardMatrix[row][col].classList.contains("danger")) {
+            areThey = true;       //assumption falsified
+        }
+    })
+    return areThey;               //return inference
+}
+
+function updateBoardAttacks() {
+    clearAttackIndicators();                //clear board indicators so the updated board have proper queen sights
+    CurrentQueens.forEach((value) => {
+        const m = value[0];
+        const n = value[1];
+        expandSight(m, n);                  //expand the sight of the current queen
     })
 }
 
-function queenSight(target, m, n) {
-    target.classList.toggle("queen");
-    if (target.classList.contains("queen")) {
-        CurrentQueens.push([m,n]);
-        // isOccupied(true, m, n);
-    }
-    else {
-        const idx=CurrentQueens.findIndex((queen)=>{
-            return queen[0]===m && queen[1]===n;
-        });
-        if(idx!==-1){
-            CurrentQueens.splice(idx,1);
-        }
-        // isOccupied(false, m, n);
-    }
-    for(let i=0;i<N;i++){
-        for(let j=0;j<N;j++){
+function clearAttackIndicators() { //simply clears all the indicators of the board entirely
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; j++) {
             BoardMatrix[i][j].classList.remove("occupiedWhite");
             BoardMatrix[i][j].classList.remove("occupiedBlack");
             BoardMatrix[i][j].classList.remove("danger");
         }
     }
-    CurrentQueens.forEach((value)=>{
-        const m=value[0];
-        const n=value[1];
-        isOccupied(m,n);
-        console.log(BoardMatrix);
-    })
-
 }
 
-function isOccupied( m, n) { //flag: A boolean for presence of queen ,(m,n): position of cell
+function expandSight(m, n) {   //expand the queen's sight as per chess rules (needs a bit optimization (future task))
 
     let i = m + 1;
     let j = n + 1;
     //expand bottom right diagonally
     while (i < N && j < N) {
-        validateFlag( i, j);
+        markAttackedCells(i, j);
         i++;
         j++;
 
@@ -132,7 +245,7 @@ function isOccupied( m, n) { //flag: A boolean for presence of queen ,(m,n): pos
 
     //expand top left diagonally
     while (i >= 0 && j >= 0) {
-        validateFlag(i, j);
+        markAttackedCells(i, j);
         i--;
         j--;
     }
@@ -141,7 +254,7 @@ function isOccupied( m, n) { //flag: A boolean for presence of queen ,(m,n): pos
     j = n + 1;
     //expand top right diagonally
     while (i >= 0 && j < N) {
-        validateFlag(i, j);
+        markAttackedCells(i, j);
         i--;
         j++;
     }
@@ -150,7 +263,7 @@ function isOccupied( m, n) { //flag: A boolean for presence of queen ,(m,n): pos
     j = n - 1;
     //expand  bottom left diagonally
     while (i < N && j >= 0) {
-        validateFlag( i, j);
+        markAttackedCells(i, j);
         i++;
         j--;
     }
@@ -159,7 +272,7 @@ function isOccupied( m, n) { //flag: A boolean for presence of queen ,(m,n): pos
     j = n;
     //expand vertically downwards
     while (i < N) {
-        validateFlag( i, j);
+        markAttackedCells(i, j);
         i++;
     }
 
@@ -167,7 +280,7 @@ function isOccupied( m, n) { //flag: A boolean for presence of queen ,(m,n): pos
     j = n;
     //expand vertically upwards
     while (i >= 0) {
-        validateFlag( i, j);
+        markAttackedCells(i, j);
         i--;
     }
 
@@ -175,7 +288,7 @@ function isOccupied( m, n) { //flag: A boolean for presence of queen ,(m,n): pos
     j = n - 1;
     //expand horizontally left
     while (j >= 0) {
-        validateFlag( i, j);
+        markAttackedCells(i, j);
         j--;
     }
 
@@ -183,16 +296,16 @@ function isOccupied( m, n) { //flag: A boolean for presence of queen ,(m,n): pos
     j = n + 1;
     //expand horizontally right
     while (j < N) {
-        validateFlag( i, j);
+        markAttackedCells(i, j);
         j++;
     }
 }
 
-function validateFlag(i, j) {
-    if(BoardMatrix[i][j].classList.contains("queen")){
+function markAttackedCells(i, j) {  //simply checks whether the current cell under consideration of the queen's sight involves an empty square or another queen
+    if (BoardMatrix[i][j].classList.contains("queen")) {
         BoardMatrix[i][j].classList.add("danger");
     }
-    else  {
+    else {
         if ((i + j) % 2 == 0) {
             BoardMatrix[i][j].classList.add("occupiedWhite");
         }
@@ -201,13 +314,20 @@ function validateFlag(i, j) {
         }
 
     }
-    // else {
-    //     if ((i + j) % 2 == 0) {
-    //         BoardMatrix[i][j].classList.remove("occupiedWhite");
-    //     }
-    //     else {
-    //         BoardMatrix[i][j].classList.remove("occupiedBlack");
-    //     }
-    // }
 
 }
+
+function calculateAndDisplayResult() {
+    let isSolved = !areQueensUnderAttack();
+    displayRightResult(isSolved); //callback to the actuall displaying function
+}
+
+function displayRightResult(isSolved) { //self-explantory
+    if (isSolved) {
+        Description.textContent = "Solved! Great Job!";
+    }
+    else {
+        Description.textContent = "Failed!";
+    }
+}
+
